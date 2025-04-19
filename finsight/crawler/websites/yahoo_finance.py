368258824b27c23e5d59cc4b6d925b4de380388a
Diego Alonso Sánchez Manríquez <diego.sanchez.m@ug.uchile.cl>
2025-04-19 02:23:50 -0400

    feat(crawler): add asynchronous Yahoo Finance news crawler using crawl4ai library

--- finsight/crawler/websites/yahoo_finance.py
+++ finsight/crawler/websites/yahoo_finance.py
@@ -1,1 +1,29 @@
-# https://finance.yahoo.com/news
+import asyncio
+
+from crawl4ai import AsyncWebCrawler
+from crawl4ai import BrowserConfig, CacheMode
+from crawl4ai.async_configs import CrawlerRunConfig
+
+
+async def yahoo_finance_crawler():
+
+    browser_config = BrowserConfig(
+        headless=False,
+    )
+
+    run_config = CrawlerRunConfig(
+        cache_mode=CacheMode.BYPASS,
+    )
+
+    async with AsyncWebCrawler(config=browser_config) as crawler:
+
+        result = await crawler.arun(
+            url="https://finance.yahoo.com/news",
+            config = run_config,
+        )
+
+        print(result.cleaned_html)
+
+
+if __name__ == "__main__":
+    asyncio.run(yahoo_finance_crawler())
