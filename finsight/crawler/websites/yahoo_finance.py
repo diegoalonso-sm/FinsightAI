ccb622e0850ffb2574bba1378de55c3485f9ba69
Diego Alonso Sánchez Manríquez <diego.sanchez.m@ug.uchile.cl>
2025-04-19 03:29:15 -0400

    feat(crawler): add deep crawling and keyword relevance scoring to yahoo finance crawler
    This update integrates a best-first crawling strategy and keyword relevance scoring for enhanced control and effectiveness of the crawling process.

--- finsight/crawler/websites/yahoo_finance.py
+++ finsight/crawler/websites/yahoo_finance.py
@@ -3,7 +3,9 @@ import asyncio
 from crawl4ai import AsyncWebCrawler
 from crawl4ai import BrowserConfig, CacheMode
 from crawl4ai.async_configs import CrawlerRunConfig
-
+from crawl4ai.content_scraping_strategy import LXMLWebScrapingStrategy
+from crawl4ai.deep_crawling import BestFirstCrawlingStrategy
+from crawl4ai.deep_crawling.scorers import KeywordRelevanceScorer
 
 async def yahoo_finance_crawler():
 
@@ -11,7 +13,20 @@ async def yahoo_finance_crawler():
         headless=True,
     )
 
+    url_scorer = KeywordRelevanceScorer(
+        keywords=["crawl", "example", "async", "configuration"],
+        weight=0.7
+    )
+
+    crawling_strategy = BestFirstCrawlingStrategy(
+        max_depth=5,
+        include_external=False,
+        url_scorer=url_scorer,
+        max_pages=25,
+    )
+
     run_config = CrawlerRunConfig(
+        #deep_crawl_strategy=crawling_strategy,
         cache_mode=CacheMode.BYPASS,
         excluded_tags=['form', 'header', 'footer', 'scripts', 'style'],
         keep_data_attributes=False,
@@ -34,6 +49,8 @@ async def yahoo_finance_crawler():
         if result.pdf:
             with open("wikipedia_page.pdf", "wb") as f:
                 f.write(result.pdf)
+
+        print(result.cleaned_html)
 
 
 if __name__ == "__main__":
