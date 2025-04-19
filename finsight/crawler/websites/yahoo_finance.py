2f1893147fa2eac4500d1e5b5f353e29af3cf5a8
Diego Alonso Sánchez Manríquez <diego.sanchez.m@ug.uchile.cl>
2025-04-19 02:40:14 -0400

    fix(yahoo_finance): update crawler config to exclude external links and remove overlay elements

--- finsight/crawler/websites/yahoo_finance.py
+++ finsight/crawler/websites/yahoo_finance.py
@@ -14,8 +14,8 @@ async def yahoo_finance_crawler():
     run_config = CrawlerRunConfig(
         cache_mode=CacheMode.BYPASS,
         excluded_tags=['form', 'header', 'footer'],
-        exclude_external_links=False,
-        remove_overlay_elements=False,
+        exclude_external_links=True,
+        remove_overlay_elements=True,
     )
 
     async with AsyncWebCrawler(config=browser_config) as crawler:
